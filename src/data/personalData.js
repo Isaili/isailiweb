93a2bdf9835880ccc56cd7f795da7b8358771e7f
Isai Abel López Sánchez <139931298+Isaili@users.noreply.github.com>
2026-08-30 21:02:19 -0600

    feature: visualizar fotos

--- src/data/personalData.js
+++ src/data/personalData.js
@@ -1,5 +1,42 @@
-import playaImage from '../assets/playa.jpg';
-import escuelaImage from '../assets/escuela.jpg';
+
+import ver2027 from '../fotos/2027.jpg';
+import ado2027 from '../fotos/ado.jpg';
+import ado2026 from '../fotos/ado2.jpeg';
+import burrto from '../fotos/burrto.jpeg'
+import cafe from '../fotos/cafe.jpeg'
+import cafe1 from '../fotos/cafe1.jpeg'
+import cafe2 from '../fotos/cafe2.jpeg'
+import caminando from '../fotos/caminando.jpeg'
+import cascadas from '../fotos/cascadas.jpeg'
+import cata from '../fotos/cata.jpeg'
+import cata2 from '../fotos/cata2.jpeg'
+import cata3 from '../fotos/cata3.jpeg'
+import catedral from '../fotos/catedral.jpeg'
+import codigo from '../fotos/codigo.jpeg'
+import doblecodigo from '../fotos/doblecodigo.jpeg'
+import famili from '../fotos/famili.jpeg'
+import iglesia from '../fotos/iglesia.jpeg'
+import lake from '../fotos/lake.jpeg'
+import lake2 from '../fotos/lake2.jpeg'
+import malecon from '../fotos/malecon.jpeg'
+import mar2 from '../fotos/mar2.jpeg'
+import me from '../fotos/me.jpeg'
+import megym from '../fotos/megym.jpeg'
+import mex from '../fotos/mex.jpeg'
+import palmeras from '../fotos/palmeras.jpeg'
+import pm2 from '../fotos/pm2.jpeg'
+import surface from '../fotos/surface.jpeg'
+import taks from '../fotos/taks.jpeg'
+import telas from '../fotos/telas.jpeg'
+import veracruz from '../fotos/veracruz.jpeg'
+import viejita from '../fotos/viejita.jpeg'
+import volcan from '../fotos/volcan.jpeg'
+import volcan2 from '../fotos/volcan2.jpeg'
+import volcan3 from '../fotos/volcan3.jpeg'
+import volcan4 from '../fotos/volcan4.jpeg'
+
+
+
 
 export const personalData = {
   name: 'Isai AbeL lOPEZ Sanchez',
@@ -87,7 +124,40 @@ export const personalData = {
     description: "azalia's films, just for me, My dreams."
   },
   coolPhotos: [
-    { src: playaImage, alt: 'Playa' },
-    { src: escuelaImage, alt: 'Uni' },
-  ],
+  { src: telas, alt: 'Telas' },
+  { src: volcan3, alt: 'Volcán' },
+  { src: me, alt: 'Yo' },
+  { src: cata2, alt: 'Cata' },
+  { src: malecon, alt: 'Malecón' },
+  { src: ado2026, alt: 'Uni' },
+  { src: iglesia, alt: 'Iglesia' },
+  { src: burrto, alt: 'Burrito' },
+  { src: pm2, alt: 'PM2' },
+  { src: ver2027, alt: 'Playa' },
+  { src: cafe2, alt: 'Café' },
+  { src: volcan, alt: 'Volcán' },
+  { src: famili, alt: 'Familia' },
+  { src: cascadas, alt: 'Cascadas' },
+  { src: catedral, alt: 'Catedral' },
+  { src: mex, alt: 'México' },
+  { src: doblecodigo, alt: 'Doble código' },
+  { src: veracruz, alt: 'Veracruz' },
+  { src: lake2, alt: 'Lago' },
+  { src: cata3, alt: 'Cata' },
+  { src: mar2, alt: 'Mar' },
+  { src: taks, alt: 'Tareas' },
+  { src: codigo, alt: 'Código' },
+  { src: ado2027, alt: 'Uni' },
+  { src: surface, alt: 'Surface' },
+  { src: volcan4, alt: 'Volcán' },
+  { src: viejita, alt: 'Viejita' },
+  { src: cafe1, alt: 'Café' },
+  { src: lake, alt: 'Lago' },
+  { src: megym, alt: 'Gimnasio' },
+  { src: palmeras, alt: 'Palmeras' },
+  { src: cata, alt: 'Cata' },
+  { src: caminando, alt: 'Caminando' },
+  { src: cafe, alt: 'Café' },
+  { src: volcan2, alt: 'Volcán' },
+],
 };
